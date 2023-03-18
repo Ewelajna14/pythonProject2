fb88ca449d51a98bc70d1ff109aea10e8f14390c
Ewelina <ekowalczyk1994@gmail.com>
2023-03-18 14:06:56 -0500

    dont allow snake to suddenly move up if is going down

--- snake.py
+++ snake.py
@@ -1,6 +1,11 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
     def __init__(self):
@@ -16,7 +21,6 @@ class Snake:
             new_segment.goto(position)
             self.segments.append(new_segment)
 
-
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
@@ -25,15 +29,19 @@ class Snake:
         self.head.forward(MOVE)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
 
